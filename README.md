c9f30b94b69c9b35c1547a93eec7f492a6fa4548
andrebalen <andrebalen@gmail.com>
2020-11-12 14:05:23 -0300

    webradioz and utilz

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bin
 toolz and scripts for my raspberryPi projects
+
+## nfo
+some scripts have dependencies
+please, run dependecies.sh to solve this
+
+# toolz
+
+
---
+++ dependencies.sh
@@ -0,0 +1,15 @@
+# run this script to install all dependecies
+# in atual version, i`ll organize altt the stuff
+echo ---------------------------------------------------------------------
+echo
+echo ---------------------------------------------------------------------
+
+echo please run 
+echo sudo apt-get update
+echo sudo apt-get install aptitude
+echo sudo aptitude install mplayer mc figlet toilet colormake terminator imagemagick 
+
+
+
+
+echo ---------------------------------------------------------------------
